4c3a97ce253fa0c71415b2b5e6e89354226de6c0
Jhini <brownlily948@gmail.com>
2026-04-13 21:41:55 +0600

    New Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Qtec Solutions</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/webp" href="/new.webp">
     @viteReactRefresh 
     @vite('resources/js/app.jsx')
 </head>
